d337b67bfa246a5bf77e7c644df0dc0db3194e24
Jarfsoft <andresjex@hotmail.com>
2021-04-05 16:07:53 -0600

    Add new task to project

--- src/createProject.js
+++ src/createProject.js
@@ -1,17 +1,23 @@
+import newTask from './createTask';
+
 export let projectsArray = [];
 
 
 class Builder{
   constructor(title){
     this.title = title
+    this.taskArray = [];
   }
   addProject(){
     projectsArray.push(this)
   }
+  addTask(){
+    this.taskArray.push(newTask());
+  }
 }
 
 export function addNewProject(){
-  const title = document.querySelector('input').value;
+  const title = document.querySelector('#project-title').value;
   const newProject = new Builder(title);
   newProject.addProject();
 }
@@ -21,7 +27,16 @@ export function printProject() {
   const projectName = document.createElement('h5');
   for (let project of projectsArray) {
     projectName.innerText = project.title;
-    projectName.classList = 'text-center text-primary bg-warning border border-light rounded m-1 p-1'
+    projectName.classList = 'text-center text-primary bg-warning border border-light rounded m-1 p-1';
+    projectName.addEventListener('click', () => {
+      const tasks = document.querySelector('#new-task-card');
+      const proj = document.querySelector('#new-project-task');
+      proj.textContent = project.title;
+      tasks.classList.remove('hide');
+      document.querySelector('#new-project-section').classList.add('hide');
+      document.querySelector('#new-project-section').classList.remove('show');
+      tasks.classList.add('show');
+    });
     projectList.appendChild(projectName);
 
   }
--- src/index.js
+++ src/index.js
@@ -2,27 +2,50 @@
 // import  createNew from './project'
 // createHome()
 import {addNewProject , projectsArray , printProject}  from './createProject';
+import newTask from './createTask';
+
+function clearInputs(){
+  document.querySelector('#task-title').value = '';
+  document.querySelector('#task-description').value = '';
+  document.querySelector('#dueDate').value = '';
+  document.querySelector('#priority').value = '';
+}
 
 const newProjectBtn = document.querySelector('#new-project-btn');
 const newProjectSection = document.querySelector('#new-project-section');
 const addProjectBtn = document.querySelector('#add-project-btn');
+const newTaskSection = document.querySelector('#new-task-card')
+const newTaskBtn = document.querySelector('#add-task-btn');
 
 // Style
 newProjectSection.classList.add('hide');
+newTaskSection.classList.add('hide');
 
 
 // Events
 newProjectBtn.addEventListener('click' , ()=>{
 newProjectSection.classList.add('show');
 newProjectSection.classList.remove('hide');
+newTaskSection.classList.remove('show');
+newTaskSection.classList.add('hide');
 } )
+
+newTaskBtn.addEventListener('click', () => {
+  clearInputs();
+  const projectName = document.querySelector('#new-project-task').innerText;
+  const project = projectsArray.find(name => name.title == projectName);
+
+  project.addTask();
+
+  console.log(project.taskArray);
+});
 
 addProjectBtn.addEventListener('click' , ()=>{
   addNewProject();
   printProject();
   newProjectSection.classList.add('hide');
   newProjectSection.classList.remove('show');
-   document.querySelector('input').value = '';
+   document.querySelector('#project-title').value = '';
 })
 
 
