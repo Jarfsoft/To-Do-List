f6421a963c9a66eba97b0f3d4e9b493cc3c6fa00
AhmedAmin90 <49597460+AhmedAmin90@users.noreply.github.com>
2021-04-05 22:27:51 +0300

    Comment unneccessary code

--- src/home.js
+++ src/home.js
@@ -1,32 +1,32 @@
 
-function createHome() {
-  // Create main elements
- const body = document.querySelector('body');
- const mainDiv = document.createElement('div');
- const btn = document.createElement('button');
- const projectsList = document.createElement('div');
- const projectLabel = document.createElement('h1');
+// function createHome() {
+//   // Create main elements
+//  const body = document.querySelector('body');
+//  const mainDiv = document.createElement('div');
+//  const btn = document.createElement('button');
+//  const projectsList = document.createElement('div');
+//  const projectLabel = document.createElement('h1');
 
- //  InnerText :
- btn.innerText = 'Create Project';
- projectLabel.innerText = 'Projects List';
-
-
-// Main classes for elements:
- mainDiv.classList = 'container d-flex justify-content-between bg-light ';
- projectsList.classList = 'border border-primary be-light d-flex flex-column project-list'
- btn.classList = 'btn btn-success'
- projectLabel.classList = 'bg-warning w-80';
+//  //  InnerText :
+//  btn.innerText = 'Create Project';
+//  projectLabel.innerText = 'Projects List';
 
 
-//  Elements structure
- body.appendChild(mainDiv);
- mainDiv.appendChild(projectsList)
- mainDiv.appendChild(btn);
- projectsList.appendChild(projectLabel)
-
-}
+// // Main classes for elements:
+//  mainDiv.classList = 'container d-flex justify-content-between bg-light ';
+//  projectsList.classList = 'border border-primary be-light d-flex flex-column project-list'
+//  btn.classList = 'btn btn-success'
+//  projectLabel.classList = 'bg-warning w-80';
 
 
+// //  Elements structure
+//  body.appendChild(mainDiv);
+//  mainDiv.appendChild(projectsList)
+//  mainDiv.appendChild(btn);
+//  projectsList.appendChild(projectLabel)
 
-export default createHome
+// }
+
+
+
+// export default createHome
